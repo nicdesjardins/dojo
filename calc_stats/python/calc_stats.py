b34390dbb855888936ef5b57016f508975926940
Nic Desjardins <nic@nicdesjardins.com>
2021-10-01 00:43:16 -0400

    Re-organizing methods for better top to bottom reading.

--- calc_stats/python/calc_stats.py
+++ calc_stats/python/calc_stats.py
@@ -2,30 +2,7 @@
 class CalcStats(object):
     def __init__(self, sequence):
         self.calcStats(sequence)
-  
-    def avg(self, arr):
-        return sum(arr) / len(arr)
-
-    def middleElement(self, arr):
-        return arr[int((len(arr)-1) / 2)]
     
-    def middleElements(self, arr):
-        return [
-            arr[int((len(arr)/2)-1)],
-            arr[int((len(arr)/2))]
-        ]
-
-    def median(self, arr):
-        sorted_arr = sorted(arr)
-        if len(sorted_arr) % 2 == 0:
-            median = self.avg(
-                self.middleElements(sorted_arr)
-            )
-        else:
-            median = self.middleElement(sorted_arr)
-        
-        return median
-
     def calcStats(self, sequence):
         print("Sequence: ", sequence)
         print("Sorted: ", sorted(sequence))
@@ -34,6 +11,28 @@ class CalcStats(object):
         print("Number of elements in the sequence: ", len(sequence))
         print("Average: ", self.avg(sequence))
         print("Median: ", self.median(sequence))
+    
+    def avg(self, arr):
+        return sum(arr) / len(arr)
+    
+    def median(self, arr):
+        sorted_arr = sorted(arr)
+        if len(sorted_arr) % 2 == 0:
+            median = self.avg(
+                self.middleElements(sorted_arr)
+            )
+        else:
+            median = self.middleElement(sorted_arr)
+        return median
+    
+    def middleElement(self, arr):
+        return arr[int((len(arr)-1) / 2)]
+    
+    def middleElements(self, arr):
+        return [
+            arr[int((len(arr)/2)-1)],
+            arr[int((len(arr)/2))]
+        ]
 
 import sys
 if __name__ == "__main__":
