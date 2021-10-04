70815f5df50e332d5f2b9ea24e12d1fb7cad9f62
nic <nic@nicdesjardins.com>
2021-10-04 09:35:48 -0400

    Re-organized CalcStats so that each stat can be accessed separately and moved the printing of stats outside the class.

--- calc_stats/python/calc_stats.py
+++ calc_stats/python/calc_stats.py
@@ -1,27 +1,54 @@
 
 class CalcStats(object):
-    def __init__(self, sequence):
-        self.calcStats(sequence)
     
-    def calcStats(self, sequence):
-        print("Sequence: ", sequence)
-        print("Sorted: ", sorted(sequence))
-        print("Minimum value: ", min(sequence))
-        print("Maximum value: ", max(sequence))
-        print("Number of elements in the sequence: ", len(sequence))
-        print("Average: ", self.avg(sequence))
-        print("Median: ", self.median(sequence))
+    __INSTRUCTIONS__ = "Please provide an array of 2 or more numbers."
+        
+    def __init__(self, sequence = None):
+        if sequence != None:
+            self.set_sequence(sequence)
     
-    def avg(self, arr):
+    def get_sequence(self):
+        return self.__sequence__
+    
+    def set_sequence(self, sequence):
+        newSequence = []
+        
+        if isinstance(sequence, list):
+            for value in sequence:
+                try:
+                    newSequence.append(float(value))
+                except Exception as ex:
+                    raise Exception(self.__INSTRUCTIONS__)
+            self.__sequence__ = newSequence
+        else:
+            raise Exception(self.__INSTRUCTIONS__)
+
+    sequence = property(get_sequence, set_sequence)
+
+    def max(self):
+        return max(self.get_sequence())
+
+    def min(self):
+        return min(self.get_sequence())
+
+    def sorted(self):
+        return sorted(self.get_sequence())
+
+    def length(self):
+        return len(self.get_sequence())
+    
+    def average(self, arr = None):
+        if arr == None:
+            arr = self.get_sequence()
         return sum(arr) / len(arr)
     
-    def median(self, arr):
-        if self.hasEvenNumberOfElements(arr):
-            return self.avg(
-                self.middleElements(sorted(arr))
+    def median(self):
+        if self.hasEvenNumberOfElements(self.get_sequence()):
+            return self.average(
+                self.middleElements(self.sorted())
             )
         else:
-            return self.middleElement(sorted(arr))
+            return self.middleElement(sorted(self.get_sequence()))
     
     def hasEvenNumberOfElements(self, arr):
         return len(arr) % 2 == 0
@@ -33,7 +60,7 @@ class CalcStats(object):
         return [
             arr[int((len(arr)/2)-1)],
             arr[int((len(arr)/2))]
-        ]
+        ]        
 
 import sys
 if __name__ == "__main__":
@@ -47,8 +74,18 @@ if __name__ == "__main__":
                 print("Please provide a list of numbers only.")
                 sys.exit(0)
     
-    if len(arr) == 0:
+    if len(arr) < 2:
         print("Please provide at least 2 numbers.")
         sys.exit(0)
     
-    CalcStats(arr)
+    try:
+        cs = CalcStats(arr)
+        print("Sequence: ", str(cs.sequence))
+        print("Sorted: ", cs.sorted())
+        print("Minimum value: ", cs.min())
+        print("Maximum value: ", cs.max())
+        print("Number of elements in the sequence: ", cs.length())
+        print("Average: ", cs.average())
+        print("Median: ", cs.median())
+    except Exception as ex:
+        print(ex)
