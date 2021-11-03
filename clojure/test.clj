4ff7f76429d5d2faac6f41e0e28123db72edfa0f
Nic Desjardins <nic@nicdesjardins.com>
2021-11-03 00:42:14 -0400

    trying out clojure

---
+++ clojure/test.clj
@@ -0,0 +1,14 @@
+; addition
+(+ 1 2)
+
+; multiplication
+(* 2 2 3)
+
+; create a function to say "Hello <name>"
+(defn greet [name] (str "Hello " name))
+
+; call greet with argument name: "Nico"
+(greet "Nico")
+
+(greet "joyo")
+
