10c44cf4605d879b7254693f29db92f06d456da8
Nic Desjardins <nic@nicdesjardins.com>
2022-01-09 12:55:49 -0500

    Have tree-struct test case working, now looking for input on how to make its definition cleaner/simpler.

--- testcases/tree-struct.c
+++ testcases/tree-struct.c
@@ -1,6 +1,9 @@
-#define SOMETING_TO_SAY 0
+#include <stdio.h>
+#include <string.h>
+
+#define SOMETHING_TO_SAY 0
 #define IS_IT_TRUE 1
-#define SAY_IT = 2
+#define SAY_IT 2
 #define KEEP_TO_YOURSELF 3
 
 struct node {
@@ -14,10 +17,41 @@ struct node {
   , { "keep it to yourself" } // 3
 };
 
-// do you have something to say?
-nodes[SOMETHING_TO_SAY].yes = &nodes[IS_IT_TRUE];
-nodes[SOMETHING_TO_SAY].no = &nodes[KEEP_TO_YOURSELF];
+void displayNode(struct node *node, int level);
+void tabs(int num, char *t);
 
-// is it true?
-nodes[IS_IT_TRUE].yes = &nodes[SAY_IT];
-nodes[IS_IT_TRUE].no = &nodes[KEEP_TO_YOURSELF];
+int main(void) {
+
+  // do you have something to say?
+  nodes[SOMETHING_TO_SAY].yes = &nodes[IS_IT_TRUE];
+  nodes[SOMETHING_TO_SAY].no = &nodes[KEEP_TO_YOURSELF];
+
+  // is it true?
+  nodes[IS_IT_TRUE].yes = &nodes[SAY_IT];
+  nodes[IS_IT_TRUE].no = &nodes[KEEP_TO_YOURSELF];
+ 
+  int level = 0;
+  displayNode(&nodes[0], level);
+  
+}
+
+void displayNode(struct node *node, int level) {
+  char t[16];
+  tabs(level, t);
+  printf("%s%s\n", t, node->text);
+
+  if(node->yes != NULL){
+    printf("\n%syes: \n", t);
+    displayNode((node->yes), level+1);
+  }
+  
+  if(node->no != NULL) {
+    printf("\n%sno: \n", t);
+    displayNode(node->no, level+1);
+  }
+}
+
+void tabs(int num, char *t) {
+  memset(t, ' ', num * 2);
+  t[num*2] = '\0';
+}
