458262bc7203f2c430623d80e14d3383d4d190dc
Nic Desjardins <nic@nicdesjardins.com>
2022-01-09 16:28:29 -0500

    Cleaned up initialization of nodes.

--- do_you_have_something_to_say/dyhsts.c
+++ do_you_have_something_to_say/dyhsts.c
@@ -1,88 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SOMETHING_TO_SAY 0
+#define IS_IT_TRUE 1
+#define IS_IT_NICE 2
+#define IS_IT_NECESSARY 3
+#define STFU 4
+#define SPEAK_W_LOVE 5
+
 struct node {
   char *text;
   struct node *yes;
   struct node *no;
 } nodes[] = {
-  { "Do you have something to say?" } // 0
-  , { "Is it true?" } // 1
-  , { "Is it nice?" } // 2
-  , { "Is it necessary?" } // 3
-  , { "S.T.F.U.", 0, 0 } // 4
-  , { "Speak with L.O.V.E.", 0, 0 } // 5
+  [SOMETHING_TO_SAY] = {
+			.text = "Do you have something to say?"
+			, .yes = &nodes[IS_IT_TRUE]
+			, .no = &nodes[STFU]
+  } 
+  , [IS_IT_TRUE] =  {
+		     .text = "is it true?"
+		     , .yes = &nodes[IS_IT_NICE]
+		     , .no = &nodes[STFU]
+  }
+  , [IS_IT_NICE] = {
+		    .text = "Is it nice?"
+		    , .yes = &nodes[SPEAK_W_LOVE]
+		    , .no = &nodes[IS_IT_NECESSARY]
+  }
+  , [IS_IT_NECESSARY] = {
+			 .text = "Is it necessary?"
+			 , .yes = &nodes[SPEAK_W_LOVE]
+			 , .no = &nodes[STFU]
+  }
+  , [SPEAK_W_LOVE] = { .text = "Speak w/ L.O.V.E." }
+  , [STFU] = { .text = "S.T.F.U." }
 };
-
-void makeNodeConnections(void);
 
 int main(void) {
 
-  makeNodeConnections();
   system("clear");
-  
-  while(1) {  
-    
-    struct node *curr;
-    curr = &nodes[0];
 
-    int c = 0;
-    while(1) {
+  struct node *curr;
+  curr = &nodes[SOMETHING_TO_SAY];
 
-      if (curr->yes == 0) {
-	printf("\n%s\n\n", curr->text);
-	//return 0;
-	break;
-      }
+  int c = 0;
+  while(1) {
 
-      if(c != '\n')
-	printf("\n%s\n(y, n, or q for quit)\n\n", curr->text);
-
-      c = getchar();
-      if (c == 'y') {
-	curr = curr->yes;
-	continue;
-      }
-
-      else if (c == 'n') {
-	curr = curr->no;
-	continue;
-      }
-
-      else if (c == 'q') {
-	printf("kthxbai!\n");
-	return 0;
-      }
-
-      else if (c == '\n') {
-	continue;
-      }
-
-      else {
-	printf("invalid option: %c\n", c);
-	continue;
-      }  
+    if (curr->yes == 0) {
+      printf("\n%s\n\n", curr->text);
+      return 0;
     }
+
+    if(c != '\n')
+      printf("\n%s\n(y, n, or q for quit)\n\n", curr->text);
+
+    c = getchar();
+    if (c == 'y') {
+      curr = curr->yes;
+      continue;
+    }
+
+    else if (c == 'n') {
+      curr = curr->no;
+      continue;
+    }
+
+    else if (c == 'q') {
+      printf("kthxbai!\n");
+      return 0;
+    }
+
+    else if (c == '\n') {
+      continue;
+    }
+
+    else {
+      printf("invalid option: %c\n", c);
+      continue;
+    }  
   }
   
   return 0;
   
-}
-
-void makeNodeConnections(void) {
-  // do you have something to say?
-  nodes[0].yes = &nodes[1]; // yes -> is it true?
-  nodes[0].no = &nodes[4]; // no -> stfu
-
-  // is it true?
-  nodes[1].yes = &nodes[2]; // yes -> is it nice?
-  nodes[1].no = &nodes[4]; // no -> stfu
-
-  // is it nice?
-  nodes[2].yes = &nodes[5]; // yes -> speak w/ love
-  nodes[2].no = &nodes[3]; // no -> is it necessary?
-
-  // is it necessary?
-  nodes[3].yes = &nodes[5]; // yes -> speak w/ love
-  nodes[3].no = &nodes[4]; // no -> stfu
 }
