66431a62d9f3f54018a955ddf8a43b13d666a64d
Nic Desjardins <nic@nicdesjardins.com>
2022-01-09 15:59:38 -0500

    Fixed up initialization of my nodes to a cleaner format.

--- testcases/tree-struct.c
+++ testcases/tree-struct.c
@@ -11,28 +11,28 @@ struct node {
   struct node *yes;
   struct node *no;
 } nodes[] = {
-  { "do you have something to say?" } // 0
-  , { "is it true?" } // 1
-  , { "say it" } // 2
-  , { "keep it to yourself" } // 3
+  [SOMETHING_TO_SAY] = {
+    .text = "do you have something to say?"
+    , .yes = &nodes[IS_IT_TRUE]
+    , .no = &nodes[KEEP_TO_YOURSELF]
+  }
+  
+  , [IS_IT_TRUE] =  {
+    .text = "is it true?"
+    , .yes = &nodes[SAY_IT]
+    , .no = &nodes[KEEP_TO_YOURSELF]
+  }
+  
+  , [SAY_IT] = { .text = "say it" }
+  , [KEEP_TO_YOURSELF] = { .text = "keep it to yourself" }
 };
 
 void displayNode(struct node *node, int level);
 void tabs(int num, char *t);
 
 int main(void) {
-
-  // do you have something to say?
-  nodes[SOMETHING_TO_SAY].yes = &nodes[IS_IT_TRUE];
-  nodes[SOMETHING_TO_SAY].no = &nodes[KEEP_TO_YOURSELF];
-
-  // is it true?
-  nodes[IS_IT_TRUE].yes = &nodes[SAY_IT];
-  nodes[IS_IT_TRUE].no = &nodes[KEEP_TO_YOURSELF];
- 
   int level = 0;
   displayNode(&nodes[0], level);
-  
 }
 
 void displayNode(struct node *node, int level) {
